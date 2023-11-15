730e055715e1e2f45109175f4cb5e136ce787e52
Jere Stigell <jj.stigell@gmail.com>
2023-11-15 21:00:46 +0200

    lang files

---
+++ assets/locales/fi.js
@@ -0,0 +1,47 @@
+const translations = {
+  settings: {
+    title: 'Settings',
+    account: 'account',
+    language: 'Language',
+    preferences: 'preferences',
+    darkMode: 'Dark Mode',
+    notifications: 'Notifications',
+    help: 'help',
+    report: 'Report Bug / Feature',
+    contact: 'Contact Us',
+    other: 'other',
+    version: 'App version: 0.0.1',
+    logout: 'Logout',
+    searchPlaceholder: 'Search...',
+    languagePlaceholder: 'Select language'
+  },
+  auth: {
+    email: 'Email',
+    password: 'Password',
+    confirmPassword: 'Confirm Password',
+    loginTitle: 'Login to existing account',
+    registerTitle: 'Create new account',
+    remember: 'Remember me',
+    alternativeLogin: 'or login with',
+    facebook: 'Facebook',
+    google: 'Google',
+    noAccount: 'Don\'t have an account?',
+    registerLink: 'Register',
+    agreeTerms: 'I agree to the ',
+    tosLink: 'terms and conditions',
+    emailField: 'Enter your email address',
+    passwordField: 'Enter your password',
+    confirmPasswordField: 'Confirm your password',
+    loginButton: 'Login',
+    loginProcess: 'Logging in, please wait...',
+    registerButton: 'Sign Up',
+    registerProcess: 'Registering, please wait...',
+    alternativeSignup: 'or sign up with',
+    alreadyAccount: 'Already have an account?',
+    loginLink: 'Login',
+    returnButton: 'Go back',
+    TermsAndConditions: 'Terms and Conditions (available only in english)'
+  }
+};
+
+export default translations;
---
+++ assets/locales/ja.js
@@ -0,0 +1,47 @@
+const translations = {
+  settings: {
+    title: '設定',
+    account: 'アカウント',
+    language: '言語',
+    preferences: '設定',
+    darkMode: 'ダークモード',
+    notifications: '通知',
+    help: 'ヘルプ',
+    report: 'バグ報告 / 機能要望',
+    contact: 'お問い合わせ',
+    other: 'その他',
+    version: 'アプリバージョン: 0.0.1',
+    logout: 'ログアウト',
+    searchPlaceholder: 'Search...',
+    languagePlaceholder: 'Select language'
+  },
+  auth: {
+    email: 'メールアドレス',
+    password: 'パスワード',
+    confirmPassword: 'パスワードの確認',
+    loginTitle: '既存のアカウントにログイン',
+    registerTitle: '新しいアカウントを作成',
+    remember: 'ログイン状態を保持する',
+    alternativeLogin: 'またはこれでログイン',
+    facebook: 'Facebook',
+    google: 'Google',
+    noAccount: 'アカウントをお持ちでないですか？',
+    registerLink: '登録する',
+    agreeTerms: '以下の条件に同意する ',
+    tosLink: '利用規約',
+    emailField: 'メールアドレスを入力',
+    passwordField: 'パスワードを入力',
+    confirmPasswordField: 'パスワードを確認',
+    loginButton: 'ログイン',
+    loginProcess: 'ログイン中です、お待ちください...',
+    registerButton: 'サインアップ',
+    registerProcess: '登録中です、お待ちください...',
+    alternativeSignup: 'またはこれでサインアップ',
+    alreadyAccount: 'すでにアカウントをお持ちですか？',
+    loginLink: 'ログイン',
+    returnButton: '戻る',
+    TermsAndConditions: '利用規約（英語のみ利用可能）'
+  }
+};
+
+export default translations;
